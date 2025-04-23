37afaa75a4d5f057bc5259186aa69d0a87c15b72
Manogna24-Tech <manogna24316@gmail.com>
2025-04-23 14:04:39 +0530

    updated

--- q27.js
+++ q27.js
@@ -1,27 +1,84 @@
 db.posts.aggregate([
-    {$lookup:{
-        from:"users",
-        localField:"userId",
-        foreignField:"_id",
-        as:"user"
-    }},
-]); //lookup query from joins in SQL - linking from posts to users 
-
-db.users.aggregate([
+    {
+      $lookup: {
+        from: "users",
+        localField: "userId",
+        foreignField: "_id",
+        as: "user",
+      },
+    },
+  ]);
+  
+  db.users.aggregate([
     {
       $lookup: {
         from: "posts",
         localField: "_id",
         foreignField: "userId",
-        as: "posts"
-      }
+        as: "posts",
+      },
+    },
+  ]);
+  
+  db.users.aggregate([
+    {
+      $lookup: {
+        from: "posts",
+        localField: "_id",
+        foreignField: "userId",
+        as: "posts",
+      },
     },
     {
       $project: {
+        _id: 0,
         name: 1,
         "posts.title": 1,
-        "posts.desc": 1
-      }
+        "posts.desc": 1,
+      },
     },
-    {$unwind: "$posts"}
   ]);
+  
+  db.users.aggregate([
+    {
+      $lookup: {
+        from: "posts",
+        localField: "_id",
+        foreignField: "userId",
+        as: "posts",
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        name: 1,
+        "posts.title": 1,
+        "posts.desc": 1,
+      },
+    },
+    { $match: { "posts.title": "title 1" } },
+  ]);
+  
+  db.users.aggregate([
+    {
+      $lookup: {
+        from: "posts",
+        let: { uid: "$_id" },
+        pipeline: [
+          { $match: { $expr: { $eq: [ "$userId","$$uid" ] } } },
+          {$match:{title:"title 1"}}
+        ],
+        as: "posts",
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        name: 1,
+        "posts.title": 1,
+        "posts.desc": 1,
+      },
+    },
+    {$unwind:"$posts"}
+  ]);
+  
